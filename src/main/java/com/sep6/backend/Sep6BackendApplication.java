8abf88d8d7e1fd072518d546c24470d5b6493f6e
Malgorzata Drygala <49656312+gosiadrygala@users.noreply.github.com>
2022-11-28 21:02:44 +0100

    Initial commit

---
+++ src/main/java/com/sep6/backend/Sep6BackendApplication.java
@@ -0,0 +1,11 @@
+package com.sep6.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep6BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Sep6BackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sep6/backend/Sep6BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep6.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep6BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
